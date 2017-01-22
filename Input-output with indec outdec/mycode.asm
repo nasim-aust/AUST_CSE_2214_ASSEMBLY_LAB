c3521f5cff44217c15b197a6d0128ceb129fa02b
MD Abdullah Al Nasim <nasim5153@gmail.com>
2017-01-22 21:08:16 +0600

    Create mycode.asm

---
+++ Input-output with indec outdec/mycode.asm
@@ -0,0 +1,23 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+MAIN PROC
+    CALL INDEC
+    PUSH AX
+    
+    MOV AH,2
+    MOV DL,0DH
+    INT 21H
+    MOV DL,0AH
+    INT 21H
+    
+    POP AX
+    CALL OUTDEC
+    
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+
+INCLUDE outdec.asm
+INCLUDE indec.asm
+END MAIN
