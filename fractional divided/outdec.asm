567db7bed49cecbe9e512becf23d801a73ecc2f3
MD Abdullah Al Nasim <nasim5153@gmail.com>
2017-01-27 17:40:07 +0600

    Create outdec.asm

---
+++ fractional divided/outdec.asm
@@ -0,0 +1,40 @@
+OUTDEC PROC
+    PUSH AX
+    PUSH BX
+    PUSH CX
+    PUSH DX
+    
+    OR AX,AX
+    JGE END_IF1
+    PUSH AX
+    MOV DL,'-'
+    MOV AH,2
+    INT 21H
+    POP AX
+    NEG AX
+    
+    END_IF1:
+    XOR CX,CX
+    MOV BX,10D
+    
+    REPEAT:
+    XOR DX,DX
+    DIV BX
+    PUSH DX
+    INC CX
+    OR AX,AX
+    JNE REPEAT
+    MOV AH,2
+    
+    PRINT_LOOP:
+    POP DX
+    OR DL,30H
+    INT 21H
+    LOOP PRINT_LOOP
+    
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+    RET
+    OUTDEC ENDP
