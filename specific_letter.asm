9f472d722be5d262905e5cfdc31b7a55e7b7dfbe
MD Abdullah Al Nasim <nasim5153@gmail.com>
2017-02-17 02:46:12 +0600

    Create specific_letter.asm

---
+++ specific_letter.asm
@@ -0,0 +1,50 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA   
+
+SUM DB 0
+
+
+.CODE
+
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    INPUT:
+    MOV AH,1
+    INT 21H 
+     CMP AL,'A'  
+     JE COM
+    
+    CMP AL,0DH
+    JNE INPUT 
+    JMP OUTPUT
+    COM:
+     INC SUM 
+     JMP INPUT
+     
+    
+  OUTPUT:  
+  MOV AH,2
+  MOV DL,0DH
+  INT 21H
+  MOV DL,0AH
+  INT 21H
+  
+  
+  ADD SUM,48
+  MOV AH,2
+  MOV DL,SUM
+  INT 21H
+  
+  MOV AH,4CH
+  INT 21H
+  
+  MAIN ENDP
+END MAIN
+
+
+
+
